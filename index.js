5e8311b8fa20e5c0e245faec34f4931cb653c6ff
Gurrehmat Oberoi <gurrehmat_oberoi@external.mckinsey.com>
2019-06-15 15:59:18 +0530

    initial repo setup

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './src/components/App/App';
+import { name as appName } from './src/components/App/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/constants/apiKeys.js
@@ -0,0 +1,2 @@
+export const CONSUMER_KEY = '4e478-d840c-8731e-be6d0-4569e-ff67d';
+export const CONSUMER_SECRET = '0228c-75979-0869f-96b44-6b21f-f4984';
